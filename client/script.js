95372ae16011fc33280203a52135ad29446ede2f
Saptak10 <saptak.bugatti20@gmail.com>
2021-09-19 13:49:59 +0530

    server client code added

--- client/script.js
+++ client/script.js
@@ -1,7 +1,25 @@
+import { io } from "socket.io-client"
+
 const joinRoomButton = document.getElementById("room-button")
 const messageInput = document.getElementById("message-input")
 const roomInput = document.getElementById("room-input") 
 const form = document.getElementById("form")
+
+const socket = io("http://localhost:3000")  // connecting the client to server port at 3000
+const userSocket = io("http://localhost:3000/user", { auth:  { token : "Test" }
+})  //to connect user or client in a specific route using the token sent on logging in
+socket.on('connect', () => {
+    displayMessage(`You connected with id: ${socket.id}`)
+    // socket.emit('custom-event', 10, 'Hi', { a: 'a' })  //emit is used to send custom events along with info we want to the server
+})  //connect event runs everytime this client connect to the server
+
+userSocket.on("connect_error", error => {   //this event sends the server the error message from client when there's an error
+    displayMessage(error)   //displays error on screen
+})
+
+socket.on('receive-message', message => {  //receives the message from client sent by the client
+    displayMessage(message)
+})
 
 form.addEventListener("submit", e => {
     e.preventDefault()
@@ -10,12 +28,16 @@ form.addEventListener("submit", e => {
 
     if (message === "") return
     displayMessage (message)
+    socket.emit('send-message', message, room )  // sends this event message and room value to server when form is submitted
 
     messageInput.value = " "
 })
 
 joinRoomButton.addEventListener("click", () => {
     const room = roomInput.value
+    socket.emit('join-room', room, message => {
+        displayMessage(message)
+    })  // sends the room id or name that client enters to join
 })
 
 function displayMessage(message) {
@@ -23,3 +45,19 @@ function displayMessage(message) {
     div.textContent = message
     document.getElementById("message-container").append(div)
 }
+
+// offline/online
+
+let count = 0
+
+setInterval(() => {
+    socket.emit('ping', ++count)    //when disconnected stores the offline sent count values and then when connected again send back the stored values of count again all at once (example receiving offline messages in whatsapp when coming online again)
+    socket.volatile.emit('ping', ++count)    //when disconnected does not stores the offline sent count values and thus when connected continue sending the new count values sent when online (example when message is sent in offline mode it cancels it and doesn't send the message)
+}, 1000)   //send to server the count value by incrementing in a delay of 1 sec 
+
+document.addEventListener('keydown',e => {
+    if(e.target.matches('input')) return
+
+    if(e.key === 'c') socket.connect()    //connect the connection from client with server ( online )
+    if(e.key === 'd') socket.disconnect()    //disconnect the connection from client with server ( offline )
+})
--- server/server.js
+++ server/server.js
@@ -1,5 +1,50 @@
-const io = require("socket.io")(3000)
-
-io.on("connection", socket => {  //connection event to connect to the client server at port 8080
-    console.log(socket.id)
+const { instrument } = require("@socket.io/admin-ui")  //socket.io admin gui
+const io = require("socket.io")(3000, {    // server running on port 3000
+    cors:{
+        origin: ["http://localhost:8080", "https://admin.socket.io/"],
+    },
 })
+
+//socket.io authentication on /user route
+
+const userIo = io.of('/user')   //to connect user or client in a specific route
+userIo.on('connection', socket => {     //namspace used in user authentication in room joining
+    console.log('connected to user namespace with username ' + socket.username)   
+})
+
+userIo.use((socket, next) => {   //getting the token sent by the user from client to the server
+    if(socket.handshake.auth.token) {    //if the token is present
+        socket.username = getUsernameFromToken(s)   //getting the username from the token
+        next()
+    } else {
+        next(new Error("Please send token"))
+    }
+})
+
+function getUsernameFromToken(token) {     //access the token from the database
+    return token
+}
+
+io.on("connection", socket => {  // connection event which runs everytime a client connects 
+    console.log(socket.id)       // to a server at port 8080 and prints the id (room id)
+    // socket.on('custom-event', (number, string, obj) => {  //receives custom events from client
+    //     console.log(number, string, obj)
+    socket.on('send-message', (message, room) => {      //receives message from client
+        if (room === '') {
+        // io.emit('receive-message', message )   //sends the received message to client back or other clients (client to client)
+        socket.broadcast.emit('receive-message', message)   //sends message to other clients excluding the current client that sends the message
+        } else {
+            socket.to(room).emit('receive-message', message)  //message sent specifically to that room id that the client are in (broadcasts by default)
+        }
+        // console.log(message)
+    })
+    socket.on('join-room', (room, cb) => {    //get the room id or name that the client enters and sends
+        socket.join(room)    //assigns the id to the new room id received and joins the room
+        cb(`Joined ${room}`)  //callback sending to the current client about the room they joined
+    })
+
+    // offline/online
+    socket.on("ping", n => console.log(n))   //receives the ping event and prints the count value
+})
+
+instrument(io, { auth : false })    //user authentication check
